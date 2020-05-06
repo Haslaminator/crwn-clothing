c294bc15270f0c8e20f967748f1431489b86c18e
Chris <Christian.Haslam.1993@gmail.com>
2020-05-06 14:25:35 +0100

    Updates

---
+++ precache-manifest.0855e1830c7a3ce2ff1093fc4740a293.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "132efed7d0131648b6c4298243db2467",
+    "url": "/crwn-clothing/index.html"
+  },
+  {
+    "revision": "17b410e9ae3ad7e4bb42",
+    "url": "/crwn-clothing/static/css/main.02aa00cf.chunk.css"
+  },
+  {
+    "revision": "0ffd49d6d28a22c98f67",
+    "url": "/crwn-clothing/static/js/2.b3442010.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/crwn-clothing/static/js/2.b3442010.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17b410e9ae3ad7e4bb42",
+    "url": "/crwn-clothing/static/js/main.c545c9b9.chunk.js"
+  },
+  {
+    "revision": "0e46efea361dabb92088",
+    "url": "/crwn-clothing/static/js/runtime-main.4e42e579.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/crwn-clothing/static/media/crown.ed28accb.svg"
+  }
+]);
