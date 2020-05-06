1dcd277ecc69458b6b84ae86b7d8ab48f823b9ca
Chris <Christian.Haslam.1993@gmail.com>
2020-05-06 16:42:15 +0100

    Updates

---
+++ precache-manifest.046f93c5d88c750b0c01e1977ffa1649.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e26f03b107a732ba36403563a4e2731f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1543abac0b59b734261d",
+    "url": "/static/css/main.d1d3b340.chunk.css"
+  },
+  {
+    "revision": "e951e5da484d769ea133",
+    "url": "/static/js/2.b0b76376.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b0b76376.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1543abac0b59b734261d",
+    "url": "/static/js/main.9a678843.chunk.js"
+  },
+  {
+    "revision": "9dce950c1f8d5f2c45b4",
+    "url": "/static/js/runtime-main.a4da908e.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/crown.ed28accb.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/crwn-clothing/precache-manifest.0855e1830c7a3ce2ff1093fc4740a293.js"
+  "/precache-manifest.046f93c5d88c750b0c01e1977ffa1649.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/crwn-clothing/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
